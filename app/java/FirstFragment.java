48b55e1b9e4338f5a74133fb69ddb7958d6f6d87
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-09-14 23:12:08 +0900

    Create FirstFragment.java

---
+++ app/java/FirstFragment.java
@@ -0,0 +1,39 @@
+package com.example.smart_mood_lamp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class FirstFragment extends Fragment {
+    private String title;
+    private int page;
+
+    public static FirstFragment newInstance(int page, String title){
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle",title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt",0);
+        title = getArguments().getString("someTitle");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        EditText tvLabel = (EditText) view.findViewById(R.id.editText);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
