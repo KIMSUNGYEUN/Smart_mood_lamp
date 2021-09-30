aab0ce4e145f65c9567e3c094daa3dff925e2489
kimsy6231 <201605ksy@dsm.hs.kr>
2021-09-30 23:07:44 +0900

    Update Light_Control.java

--- app/java/Light_Control.java
+++ app/java/Light_Control.java
@@ -1,5 +1,6 @@
 package com.example.smart_mode_lampes;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,9 +11,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Light_Control extends AppCompatActivity {
+    public static Activity secondActivity;
+
+    private BackPressHandler backPressHandler = new BackPressHandler(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        secondActivity = Light_Control.this;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.light_control);
 
@@ -24,21 +31,27 @@ public class Light_Control extends AppCompatActivity {
                     case R.id.page_home:
                         Intent intent0 = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent0);
+                        finish();
                         return true;
+
                     case R.id.page_control:
-                        Intent intent = new Intent(getApplicationContext(), Light_Control.class);
-                        startActivity(intent);
-                        //getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentLight).commit();
                         return true;
+
                     case R.id.page_status:
                         Intent intent2 = new Intent(getApplicationContext(), Show_Status.class);
                         startActivity(intent2);
-                        //getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentStatus).commit();
+                        finish();
                         return true;
                 }
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Default
+        backPressHandler.onBackPressed();
     }
 
 }
