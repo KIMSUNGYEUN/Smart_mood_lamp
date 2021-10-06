174aee6c0826908fef22070c94c3b0f284eff328
kimsy030 <201605ksy@dsm.hs.kr>
2021-10-06 20:25:31 +0900

    Update Light_Control.java

--- app/java/Light_Control.java
+++ app/java/Light_Control.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Light_Control extends AppCompatActivity {
+    int status = 0;
+    ConnectedThread connectedThread;
     public static Activity secondActivity;
 
     private BackPressHandler backPressHandler = new BackPressHandler(this);
@@ -52,6 +56,45 @@ public class Light_Control extends AppCompatActivity {
     public void onBackPressed() {
         // Default
         backPressHandler.onBackPressed();
+    }
+
+    public void onClickButtonSend1(View view){
+        if(connectedThread!=null && status != 1){
+            connectedThread.write("m");
+            Toast.makeText(getApplicationContext(), "캠핑모드로 설정합니다.", Toast.LENGTH_SHORT).show();
+            status = 1;
+        }
+        else if(connectedThread!=null && status != 0){
+            connectedThread.write("c");
+            Toast.makeText(getApplicationContext(), "캠핑모드를 중지합니다.", Toast.LENGTH_SHORT).show();
+            status = 0;
+        }
+    }
+
+    public void onClickButtonSend2(View view){
+        if(connectedThread!=null && status != 1){
+            connectedThread.write("s");
+            Toast.makeText(getApplicationContext(), "수면모드로 설정합니다.", Toast.LENGTH_SHORT).show();
+            status = 1;
+        }
+        else if(connectedThread!=null && status != 0){
+            connectedThread.write("c");
+            Toast.makeText(getApplicationContext(), "수면모드를 중지합니다.", Toast.LENGTH_SHORT).show();
+            status = 0;
+        }
+    }
+
+    public void onClickButtonSend3(View view){
+        if(connectedThread!=null && status != 1){
+            connectedThread.write("r");
+            Toast.makeText(getApplicationContext(), "독서모드로 설정합니다.", Toast.LENGTH_SHORT).show();
+            status = 1;
+        }
+        else if(connectedThread!=null && status != 0){
+            connectedThread.write("c");
+            Toast.makeText(getApplicationContext(), "독서모드를 중지합니다.", Toast.LENGTH_SHORT).show();
+            status = 0;
+        }
     }
 
 }
