080b8236a4c66e77ce5ead81ab4fe0e15af26696
kimsy030 <201605ksy@dsm.hs.kr>
2021-10-15 22:37:21 +0900

    Update SplashActivity.java

--- app/java/SplashActivity.java
+++ app/java/SplashActivity.java
@@ -1,1 +1,15 @@
 //로딩 화면
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
