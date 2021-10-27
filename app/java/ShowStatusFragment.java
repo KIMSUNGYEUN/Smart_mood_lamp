12e5d57813cf215c0aa64b4afe70ee17e2d3480c
kimsy030 <201605ksy@dsm.hs.kr>
2021-10-27 23:07:24 +0900

    Update and rename Show_Status.java제거 to ShowStatusFragment.java

--- app/java/ShowStatusFragment.java
+++ app/java/ShowStatusFragment.java
@@ -1,4 +1,57 @@
 package com.example.smart_mode_lampes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ShowStatusFragment extends BaseFragment {
+
+    TextView textView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_show_status, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textView = view.findViewById(R.id.textView);
+
+        SeekBar seekBar = view.findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView.setText(String.format("현재 선택된 밝기는 %d 입니다.", seekBar
+                        .getProgress()));
+            }
+        });
+    }
+}
+
+
+/*
+package com.example.smart_mode_lampes;
 //사용자 밝기 설정
 import android.app.Activity;
 import android.content.Intent;
@@ -82,3 +135,4 @@ public class Show_Status extends AppCompatActivity {
     }
 
 }
+*/
