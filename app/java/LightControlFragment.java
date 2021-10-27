6afd3a488c58a186eb6c86ae63a445ccae620abc
kimsy030 <201605ksy@dsm.hs.kr>
2021-10-27 23:06:41 +0900

    Update and rename Light_Control.java제거 to LightControlFragment.java

--- app/java/LightControlFragment.java
+++ app/java/LightControlFragment.java
@@ -1,5 +1,82 @@
 package com.example.smart_mode_lampes;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class LightControlFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_light_control, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btn_control1).setOnClickListener(this::onClickButtonSend1);
+        view.findViewById(R.id.btn_control2).setOnClickListener(this::onClickButtonSend2);
+        view.findViewById(R.id.btn_control3).setOnClickListener(this::onClickButtonSend3);
+    }
+
+    public void onClickButtonSend1(View view) {
+        ConnectedThread connectedThread = getConnectedThread();
+
+        if (connectedThread != null && model.getStatus().getValue() != 1) {
+            connectedThread.write("m");
+            Toast.makeText(requireActivity(), "캠핑모드로 설정합니다.", Toast.LENGTH_SHORT).show();
+            model.setStatus(1);
+
+        } else if (connectedThread != null && model.getStatus().getValue() != 0) {
+            connectedThread.write("c");
+            Toast.makeText(requireActivity(), "캠핑모드를 중지합니다.", Toast.LENGTH_SHORT).show();
+            model.setStatus(0);
+        }
+    }
+
+    public void onClickButtonSend2(View view) {
+        ConnectedThread connectedThread = getConnectedThread();
+
+        if (connectedThread != null && model.getStatus().getValue() != 1) {
+            connectedThread.write("s");
+            Toast.makeText(requireActivity(), "수면모드로 설정합니다.", Toast.LENGTH_SHORT).show();
+            model.setStatus(1);
+
+        } else if (connectedThread != null && model.getStatus().getValue() != 0) {
+            connectedThread.write("c");
+            Toast.makeText(requireActivity(), "수면모드를 중지합니다.", Toast.LENGTH_SHORT).show();
+            model.setStatus(0);
+        }
+    }
+
+    public void onClickButtonSend3(View view) {
+        ConnectedThread connectedThread = getConnectedThread();
+
+        if (connectedThread != null && model.getStatus().getValue() != 1) {
+            connectedThread.write("r");
+            Toast.makeText(requireActivity(), "독서모드로 설정합니다.", Toast.LENGTH_SHORT).show();
+            model.setStatus(1);
+
+        } else if (connectedThread != null && model.getStatus().getValue() != 0) {
+            connectedThread.write("c");
+            Toast.makeText(requireActivity(), "독서모드를 중지합니다.", Toast.LENGTH_SHORT).show();
+            model.setStatus(0);
+        }
+    }
+}
+
+
+/*
+package com.example.smart_mode_lampes;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -98,3 +175,4 @@ public class Light_Control extends AppCompatActivity {
     }
 
 }
+*/
