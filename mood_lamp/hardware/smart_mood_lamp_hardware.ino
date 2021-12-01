d8a30de640e5ecaa18dfa092168c1ac43015fdec
kimsy030 <201605ksy@dsm.hs.kr>
2021-12-01 20:28:24 +0900

    Update and rename smart_mood_lamp_hardware to smart_mood_lamp_hardware.ino

--- mood_lamp/hardware/smart_mood_lamp_hardware.ino
+++ mood_lamp/hardware/smart_mood_lamp_hardware.ino
@@ -11,7 +11,8 @@
 //세번째 인자값은 네오픽셀의 타입에 따라 바뀌는 flag
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDNUM, PIN, NEO_GRBW + NEO_KHZ800);
 
-
+String Str = "";
+char data;
 int RX=7;
 int TX=8;
 
@@ -24,74 +25,51 @@ void setup(){
 }
 /*
 void loop(){
-  Str = "";
+  strip.begin();                           // 네오픽셀 제어시작
+  strip.show();      
     while(bluetooth.available()) { //if received Bluetooth data
-        char buffer0 = (char)bluetooth.read(); //save char in buffer
-        Str += buffer0; //save buffer data to Str
-        if (buffer0 == '\n') {   //print Str at enter
-          Serial.print(Str);*/
-          /*
-          if(Str == "o"){
-            data = 50;
-            neopixel(data);
-          }
-          else if(Str == "c"){
-            data = 100;
-            neopixel(data);
-          }
-          //캠핑모드
-          if(Str == "m"){
-            data = 130;
-            neopixel(data);
-          }
-          else if(Str == "mc"){
-            data = 150;
-            neopixel(data);
-          }
-          //수면모드
-          if(data == "s"){
-            data = 170;
-            neopixel(data);
-          }
-          else if(data == "sc"){
-            data = 190;
-            neopixel(data);
-          }
-          //독서모드
-          if(data == "r"){
-            data = 210;
-            neopixel(data);
-          }
-          else if(data == "rc"){
-            data = 250;
-            neopixel(data);
-          }
-          delay(200);
-        }
-     }
-}*/
-/*
-void neopixel(int data)
-{
-       strip.setPixelColor(0, 0, 0, 0, data);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
-       strip.setPixelColor(1, 0, 0, 0, data);
-       strip.setPixelColor(2, 0, 0, 0, data);
-       strip.setPixelColor(3, 0, 0, 0, data);
-       strip.setPixelColor(4, 0, 0, 0, data);
-       strip.setPixelColor(5, 0, 0, 0, data);
-       strip.setPixelColor(6, 0, 0, 0, data);
-       strip.setPixelColor(7, 0, 0, 0, data);
+        char mychar = (char)bluetooth.read(); //save char in buffer
+        Str += mychar; //save buffer data to Str
+        delay(100);
+    }
+    if (!Str.equals("")) {   //print Str at enter
+            Serial.println(Str);
+
+             if(Str == "o"){
+                strip.setPixelColor(0, 0, 0, 0, 200);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
+                strip.setPixelColor(1, 0, 0, 0, 200);
+                strip.setPixelColor(2, 0, 0, 0, 200);
+                strip.setPixelColor(3, 0, 0, 0, 200);
+                strip.setPixelColor(4, 0, 0, 0, 200);
+                strip.setPixelColor(5, 0, 0, 0, 200);
+                strip.setPixelColor(6, 0, 0, 0, 200);
+                strip.setPixelColor(7, 0, 0, 0, 200);
+             }
+             else if(Str == "c"){
+                strip.setPixelColor(0, 0, 0, 0, 0);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
+                strip.setPixelColor(1, 0, 0, 0, 0);
+                strip.setPixelColor(2, 0, 0, 0, 0);
+                strip.setPixelColor(3, 0, 0, 0, 0);
+                strip.setPixelColor(4, 0, 0, 0, 0);
+                strip.setPixelColor(5, 0, 0, 0, 0);
+                strip.setPixelColor(6, 0, 0, 0, 0);
+                strip.setPixelColor(7, 0, 0, 0, 0);
+           
+            }
+          Str = "";
+    }
 }*/
 
 void loop() {
-  strip.begin();                           // 네오픽셀 제어시작
-  strip.show();                            // 네오픽셀 초기화
-  while(bluetooth.available()){
-    char data = bluetooth.read();
-    //Serial.print(data); // 시리얼 모니터에 데이터 출력
+  if(bluetooth.available()){
+    data = bluetooth.read();
+    delay(5);
+    strip.begin();                           // 네오픽셀 제어시작
+    strip.show();                            // 네오픽셀 초기화
+    Serial.write(data); // 시리얼 모니터에 데이터 출력
     //자동모드
     
-    if(data == 'o'){
+    if(data == 'c'){
         strip.setPixelColor(0, 0, 0, 0, 200);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
         strip.setPixelColor(1, 0, 0, 0, 200);
         strip.setPixelColor(2, 0, 0, 0, 200);
@@ -101,7 +79,7 @@ void loop() {
         strip.setPixelColor(6, 0, 0, 0, 200);
         strip.setPixelColor(7, 0, 0, 0, 200);
     }
-    else if(data == 'c'){
+    else if(data == 'o'){
        strip.setPixelColor(0, 0, 0, 0, 0);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
        strip.setPixelColor(1, 0, 0, 0, 0);
        strip.setPixelColor(2, 0, 0, 0, 0);
@@ -123,14 +101,14 @@ void loop() {
        strip.setPixelColor(7, 0, 0, 0, 150);
     }
     else if(data == 'n'){
-       strip.setPixelColor(0, 0, 0, 0, 170);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
-       strip.setPixelColor(1, 0, 0, 0, 170);
-       strip.setPixelColor(2, 0, 0, 0, 170);
-       strip.setPixelColor(3, 0, 0, 0, 170);
-       strip.setPixelColor(4, 0, 0, 0, 170);
-       strip.setPixelColor(5, 0, 0, 0, 170);
-       strip.setPixelColor(6, 0, 0, 0, 170);
-       strip.setPixelColor(7, 0, 0, 0, 170);
+       strip.setPixelColor(0, 0, 0, 0, 10);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
+       strip.setPixelColor(1, 0, 0, 0, 10);
+       strip.setPixelColor(2, 0, 0, 0, 10);
+       strip.setPixelColor(3, 0, 0, 0, 10);
+       strip.setPixelColor(4, 0, 0, 0, 10);
+       strip.setPixelColor(5, 0, 0, 0, 10);
+       strip.setPixelColor(6, 0, 0, 0, 10);
+       strip.setPixelColor(7, 0, 0, 0, 10);
         
     }
     //수면모드
@@ -144,8 +122,8 @@ void loop() {
        strip.setPixelColor(6, 0, 0, 0, 190);
        strip.setPixelColor(7, 0, 0, 0, 190);
     }
-    else if(data == 'x'){
-        strip.setPixelColor(0, 0, 0, 0, 10);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
+    else if(data == 'l'){
+       strip.setPixelColor(0, 0, 0, 0, 10);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
        strip.setPixelColor(1, 0, 0, 0, 10);
        strip.setPixelColor(2, 0, 0, 0, 10);
        strip.setPixelColor(3, 0, 0, 0, 10);
@@ -165,8 +143,8 @@ void loop() {
        strip.setPixelColor(6, 0, 0, 0, 210);
        strip.setPixelColor(7, 0, 0, 0, 210);
     }
-    else if(data == 'f'){
-        strip.setPixelColor(0, 0, 0, 0, 10);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
+    else if(data == 'd'){
+       strip.setPixelColor(0, 0, 0, 0, 10);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
        strip.setPixelColor(1, 0, 0, 0, 10);
        strip.setPixelColor(2, 0, 0, 0, 10);
        strip.setPixelColor(3, 0, 0, 0, 10);
@@ -210,16 +188,6 @@ void loop() {                              // 이 안에 입력한 내용들이 
 //블루투스 테스트
 /*
 #include <SoftwareSerial.h>
-#include <Adafruit_NeoPixel.h>
-
-#define PIN 6                      // 디지털핀 어디에 연결했는지 입력
-#define LEDNUM 8                  // 연결된 네오픽셀의 숫자입력
-#define BRIGHTNESS 50               // 네오픽셀의 밝기를 설정합니다. (0~255)
-//첫번째 인자값은 네오픽셀의 LED의 개수
-//두번째 인자값은 네오픽셀이 연결된 아두이노의 핀번호
-//세번째 인자값은 네오픽셀의 타입에 따라 바뀌는 flag
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDNUM, PIN, NEO_GRBW + NEO_KHZ800);
-
 
 int RX=7;
 int TX=8;
@@ -229,42 +197,9 @@ SoftwareSerial bluetooth(RX, TX);
 void setup(){
   Serial.begin(9600);
   bluetooth.begin(9600);
-  strip.setBrightness(BRIGHTNESS);
 }
- 
-void loop(){
-  strip.begin();                           // 네오픽셀 제어시작
-  strip.show();                            // 네오픽셀 초기화
-  
-  Str = "";
-   while(bluetooth.available()) { //if received Bluetooth data
-      char buffer0 = (char)bluetooth.read(); //save char in buffer
-      Str += buffer0; //save buffer data to Str
-      if (buffer0 == '\n') {   //print Str at enter
-        Serial.println(Str);
-        if(Str == "o"){
-          strip.setPixelColor(0, 0, 0, 0, 200);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
-          strip.setPixelColor(1, 0, 0, 0, 200);
-          strip.setPixelColor(2, 0, 0, 0, 200);
-          strip.setPixelColor(3, 0, 0, 0, 200);
-          strip.setPixelColor(4, 0, 0, 0, 200);
-          strip.setPixelColor(5, 0, 0, 0, 200);
-          strip.setPixelColor(6, 0, 0, 0, 200);
-          strip.setPixelColor(7, 0, 0, 0, 200);
-        }
-        else if(Str == "oc"){
-          strip.setPixelColor(0, 0, 0, 0, 0);     // (A,R,G,B,W) A번째 LED를 RGBW (0~255) 만큼의 밝기로 켭니다.
-          strip.setPixelColor(1, 0, 0, 0, 0);
-          strip.setPixelColor(2, 0, 0, 0, 0);
-          strip.setPixelColor(3, 0, 0, 0, 0);
-          strip.setPixelColor(4, 0, 0, 0, 0);
-          strip.setPixelColor(5, 0, 0, 0, 0);
-          strip.setPixelColor(6, 0, 0, 0, 0);
-          strip.setPixelColor(7, 0, 0, 0, 0);
-        }
-      }
-   }
-   
+
+void loop(){  
   if (bluetooth.available()) {
     Serial.write(bluetooth.read());
   }
